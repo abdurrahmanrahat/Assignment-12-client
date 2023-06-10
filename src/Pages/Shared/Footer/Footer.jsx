366036c3c2616a48d311beae6e7850d7cb3f35bd
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-06-10 18:50:09 +0600

    Navbar and Footer Worked

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,9 +1,52 @@
-
+import Logo from '../../../assets/logo.png';
+import { FaFacebook, FaTwitter, FaInstagramSquare, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <div>
-            <h2>Footer</h2>
+            <footer className="footer p-10 pb-4 bg-base-200 text-base-content">
+                <div>
+                    <div className='flex items-center'>
+                        <img className='w-24' src={Logo} alt="" />
+                        <h2 className='text-4xl font-medium'>FLLS</h2>
+                    </div>
+                    <p>Foreign Language Learning School<br />Teaching language skill since 2007.</p>
+                </div>
+                <div>
+                    <span className="footer-title text-xl">Contact Info</span>
+                    <div className='flex flex-col gap-2'>
+                        <p>Phone: 0088-3674-3972</p>
+                        <p>Phone: 0088-5599-4532</p>
+                        <p>Email: info@example.com</p>
+                    </div>
+                </div>
+                <div>
+                    <span className="footer-title text-xl">Social Links</span>
+                    <div className='flex items-center gap-3 text-md'>
+                        <FaFacebook></FaFacebook> Facebook
+                    </div>
+                    <div className='flex items-center gap-3 text-md'>
+                        <FaTwitter></FaTwitter> Twitter
+                    </div>
+                    <div className='flex items-center gap-3 text-md'>
+                        <FaInstagramSquare></FaInstagramSquare> Instagram
+                    </div>
+                    <div className='flex items-center gap-3 text-md'>
+                        <FaLinkedin></FaLinkedin> Linkedin
+                    </div>
+                </div>
+                <div>
+                    <span className="footer-title text-xl">Address</span>
+                    <p>123 Main Street <br />
+                        Anytown, USA 12345
+                    </p>
+                </div>
+            </footer>
+
+            {/* Copyright */}
+            <div className="text-center pb-2 bg-base-200">
+                <p className='text-center m-0 text-xl'><small>© copyright of my design.</small></p>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,8 +1,45 @@
+import { Link } from 'react-router-dom';
+import Logo from '../../../assets/logo.png'
 
 const Header = () => {
+
+
+    const navLists = <>
+        <li className='text-lg'><Link to='/'>Home</Link></li>
+        <li className='text-lg'><Link to='/instructors'>Instructors</Link></li>
+        <li className='text-lg'><Link to='/classes'>Classes</Link></li>
+        {/* {
+            user && <li><Link to='/dashboard '>Dashboard </Link></li>
+        } */}
+    </>
+
     return (
-        <div>
-            <h2>Header</h2>
+        <div className="navbar bg-base-200">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLists}
+                    </ul>
+                </div>
+                <img className='w-20' src={Logo} alt="" />
+                <h2 className='text-4xl font-medium'>FLLS</h2>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navLists}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {/* {
+                    user && <img src={} alt="" />
+                } */}
+                <Link to='/login'>
+                    <button className="btn btn-active text-lg bg-[#FFBD00] hover:bg-[#0E0C1A] text-black hover:text-white">Login</button>
+                </Link>
+            </div>
         </div>
     );
 };
