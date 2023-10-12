872006e5a6885d22a77e9f9233b5f6eaa7ae5cad
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-12 11:13:00 +0600

    Navbar modified

--- src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -3,142 +3,144 @@ import { useEffect } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import Swal from "sweetalert2";
 
-
 const ManageUsers = () => {
-    const [users, setUsers] = useState();
+  const [users, setUsers] = useState();
 
-    useEffect(() => {
-        fetch('https://assignment-12-server-lyart-xi.vercel.app/users', {
-            method: 'GET'
-        })
-            .then(res => res.json())
-            .then(data => setUsers(data))
-    }, [])
+  useEffect(() => {
+    fetch("https://assignment-12-server-lyart-xi.vercel.app/users", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
 
-
-    // Handle make admin 
-    const handleMakeAdmin = user => {
-
-        if (user.role === 'admin') {
+  // Handle make admin
+  const handleMakeAdmin = (user) => {
+    if (user.role === "admin") {
+      Swal.fire({
+        title: "Error!",
+        text: `${user.name} is already admin!`,
+        icon: "error",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return;
+    } else {
+      fetch(
+        `https://assignment-12-server-lyart-xi.vercel.app/users/admin/${user._id}`,
+        {
+          method: "PATCH",
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.modifiedCount) {
+            // refetch();
             Swal.fire({
-                title: 'Error!',
-                text: `${user.name} is already admin!`,
-                icon: 'error',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            return;
-        }
-        else {
-            fetch(`https://assignment-12-server-lyart-xi.vercel.app/users/admin/${user._id}`, {
-                method: 'PATCH'
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-                    if (data.modifiedCount) {
-                        // refetch();
-                        Swal.fire({
-                            title: 'Success!',
-                            text: `${user.name} is now Admin!`,
-                            icon: 'success',
-                            showConfirmButton: false,
-                            timer: 1500
-                        })
-                    }
-                })
-        }
+              title: "Success!",
+              text: `${user.name} is now Admin!`,
+              icon: "success",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
     }
+  };
 
-    // Handle Make Instructor
-    const handleMakeInstructor = user => {
-        console.log(user);
+  // Handle Make Instructor
+  const handleMakeInstructor = (user) => {
+    console.log(user);
 
-        if (user.role === 'instructor') {
+    if (user.role === "instructor") {
+      Swal.fire({
+        title: "Error!",
+        text: `${user.name} is already Instructor!`,
+        icon: "error",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return;
+    } else {
+      fetch(
+        `https://assignment-12-server-lyart-xi.vercel.app/users/instructor/${user._id}`,
+        {
+          method: "PATCH",
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.modifiedCount) {
+            // refetch();
             Swal.fire({
-                title: 'Error!',
-                text: `${user.name} is already Instructor!`,
-                icon: 'error',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            return;
-        }
-        else {
-            fetch(`https://assignment-12-server-lyart-xi.vercel.app/users/instructor/${user._id}`, {
-                method: 'PATCH'
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-                    if (data.modifiedCount) {
-                        // refetch();
-                        Swal.fire({
-                            title: 'Success!',
-                            text: `${user.name} is now an Instructor!`,
-                            icon: 'success',
-                            showConfirmButton: false,
-                            timer: 1500
-                        })
-                    }
-                })
-        }
+              title: "Success!",
+              text: `${user.name} is now an Instructor!`,
+              icon: "success",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
     }
+  };
 
-    return (
-        <div>
-            {/* Title */}
-            <SectionTitle
-                subHeading='Here are listed'
-                heading='All Users'
-            ></SectionTitle>
+  return (
+    <div>
+      {/* Title */}
+      <SectionTitle
+        subHeading="Here are listed"
+        heading="All Users"
+      ></SectionTitle>
 
-            {/* Table Here */}
-            <div className="overflow-x-auto mb-20">
-                <table className="table shadow-lg text-white text-lg">
-                    {/* head */}
-                    <thead className="py-2">
-                        <tr className="bg-gradient-to-r from-[#0a0115e7] to-[#0b0016] text-white text-lg">
-                            <th className="py-4">#</th>
-                            <th className="text-left">UserName</th>
-                            <th className="text-left">UserEmail</th>
-                            <th className="text-left">Role</th>
-                            <th className="text-left">Make Instructor</th>
-                            <th className="text-left">Make Admin</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users?.map((user, index) => <tr
-                                key={index}
-                                className="bg-gradient-to-r from-[#F75DC8] to-[#FFBD00] text-white">
-                                <th>
-                                    {index + 1}
-                                </th>
-                                <td>
-                                    {user.name}
-                                </td>
-                                <td>
-                                    {user.email}
-                                </td>
-                                <td className="text-lg">
-                                    {
-                                        user.role ? user.role : 'student'
-                                    }
-                                </td>
-                                <td>
-                                    <button onClick={() => handleMakeInstructor(user)} className="btn btn-ghost btn-sm bg-[#F75DC8] hover:bg-[#0E0C1A] text-white hover:text-white">Instructor</button>
-                                </td>
-                                <td>
-                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost btn-sm bg-[#F75DC8] hover:bg-[#0E0C1A] text-white hover:text-white">Admin</button>
-                                </td>
-                            </tr>)
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+      {/* Table Here */}
+      <div className="overflow-x-auto mb-20">
+        <table className="table shadow-lg text-white text-lg">
+          {/* head */}
+          <thead className="py-2">
+            <tr className="bg-gradient-to-r from-[#0a0115e7] to-[#0b0016] text-white text-lg">
+              <th className="py-4">#</th>
+              <th className="text-left">UserName</th>
+              <th className="text-left">UserEmail</th>
+              <th className="text-left">Role</th>
+              <th className="text-left">Make Instructor</th>
+              <th className="text-left">Make Admin</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map((user, index) => (
+              <tr
+                key={index}
+                className="bg-gradient-to-r from-[#F75DC8] to-[#FFBD00] text-white"
+              >
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td className="text-lg">{user.role ? user.role : "student"}</td>
+                <td>
+                  <button
+                    onClick={() => handleMakeInstructor(user)}
+                    className="btn btn-ghost btn-sm bg-[#F75DC8] hover:bg-[#0E0C1A] text-white hover:text-white"
+                  >
+                    Instructor
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleMakeAdmin(user)}
+                    className="btn btn-ghost btn-sm bg-[#F75DC8] hover:bg-[#0E0C1A] text-white hover:text-white"
+                  >
+                    Admin
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageUsers;
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
-import Logo from "../../../assets/logo.png";
+import NonUserPhoto from "../../../assets/non-user-photo.png";
 import { useContext } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
 import { useState } from "react";
 import { useEffect } from "react";
 // import ThemeToggle from "../../../components/ThemeToggle/ThemeToggle";
 import DarkMoodToggle from "../../../components/DarkMoodToggle/DarkMoodToggle";
+import ActiveLink from "./ActiveLink";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 const Header = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
   const [users, setUsers] = useState();
 
   const { user, logOut } = useContext(AuthContext);
@@ -28,34 +32,9 @@ const Header = () => {
   const isInstructor = matchUserRole === "instructor";
   const isStudent = matchUserRole === "student";
 
-  const navLists = (
-    <>
-      <li className="text-xl">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="text-xl">
-        <Link to="/instructors">Instructors</Link>
-      </li>
-      <li className="text-xl">
-        <Link to="/classes">Classes</Link>
-      </li>
-      {isAdmin && (
-        <li className="text-xl">
-          <Link to="/dashboard/manageclasses">Dashboard </Link>
-        </li>
-      )}
-      {isInstructor && (
-        <li className="text-xl">
-          <Link to="/dashboard/myclasses">Dashboard </Link>
-        </li>
-      )}
-      {isStudent && (
-        <li className="text-xl">
-          <Link to="/dashboard/myselectedclass">Dashboard </Link>
-        </li>
-      )}
-    </>
-  );
+  // const navLists = (
+  //   <></>
+  // );
 
   // Handle Logout
   const handleLogout = () => {
@@ -65,66 +44,141 @@ const Header = () => {
   };
 
   return (
-    <div className="navbar fixed z-10 bg-opacity-40 bg-black max-w-screen-xl text-white">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {navLists}
-          </ul>
-        </div>
+    <div className="bg-gray-900 text-white px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen md:px-24 lg:px-8">
+      <div className="flex items-center justify-between relative">
+        {/* logo section */}
         <Link to="/">
-          <img className="w-14" src={Logo} alt="" />
+          <h1 className="text-3xl font-bold">
+            <span className="text-[#EE9322]">Foreign</span>
+            <span className="text-[#9875ff]">Tone</span>
+          </h1>
         </Link>
-        <h2 className="text-4xl font-medium ml-2">FLLS</h2>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{navLists}</ul>
-      </div>
-      <div className="navbar-end">
-        <span className="hidden md:block mr-4">
-          {/* <ThemeToggle></ThemeToggle> */}
-          <DarkMoodToggle></DarkMoodToggle>
-        </span>
-        {user ? (
-          <>
-            <img
-              className="w-12 rounded-full mr-4"
-              src={user.photoURL}
-              alt=""
-            />
-            <button
-              onClick={handleLogout}
-              className="btn btn-active text-lg bg-[#FFBD00] hover:bg-[#0E0C1A] text-white border-none"
-            >
-              logOut
-            </button>
-          </>
-        ) : (
-          <Link to="/login">
-            <button className="btn btn-active text-lg bg-[#FFBD00] hover:bg-[#0E0C1A] text-white border-none">
-              Login
-            </button>
-          </Link>
-        )}
+
+        {/* Nav items section */}
+        <ul className="hidden lg:flex space-x-5 text-[17px]">
+          <li>
+            <ActiveLink to="/">Home</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink to="/instructors">Instructors</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink to="/classes">Classes</ActiveLink>
+          </li>
+          {isAdmin && (
+            <li>
+              <ActiveLink to="/dashboard/manageclasses">Dashboard</ActiveLink>
+            </li>
+          )}
+          {isInstructor && (
+            <li>
+              <ActiveLink to="/dashboard/myclasses">Dashboard</ActiveLink>
+            </li>
+          )}
+          {isStudent && (
+            <li>
+              <ActiveLink to="/dashboard/myselectedclass">Dashboard</ActiveLink>
+            </li>
+          )}
+        </ul>
+
+        {/* Mobile menu */}
+        <div className="lg:hidden">
+          {/* toggle open icon */}
+          <div onClick={() => setIsOpenMenu(true)}>
+            <GiHamburgerMenu className="w-12 h-10 text-[#EE9322] p-2 ms-1 " />
+          </div>
+
+          {isOpenMenu && (
+            <div className="absolute top-0 left-0 w-full z-10">
+              <div className="p-5 bg-gray-950 rounded shadow-sm">
+                {/* logo and close toggle icon */}
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <Link to="/">
+                      <h1 className="text-3xl font-bold">
+                        <span className="text-[#EE9322]">Foreign</span>
+                        <span className="text-[#9875ff]">Tone</span>
+                      </h1>
+                    </Link>
+                  </div>
+                  <div onClick={() => setIsOpenMenu(false)}>
+                    <IoMdClose className="h-8 w-8 text-white" />
+                  </div>
+                </div>
+
+                {/* mobile nav items */}
+                <nav>
+                  <ul className="space-y-4">
+                    <li>
+                      <ActiveLink to="/">Home</ActiveLink>
+                    </li>
+                    <li>
+                      <ActiveLink to="/instructors">Instructors</ActiveLink>
+                    </li>
+                    <li>
+                      <ActiveLink to="/classes">Classes</ActiveLink>
+                    </li>
+                    {isAdmin && (
+                      <li>
+                        <ActiveLink to="/dashboard/manageclasses">
+                          Dashboard
+                        </ActiveLink>
+                      </li>
+                    )}
+                    {isInstructor && (
+                      <li>
+                        <ActiveLink to="/dashboard/myclasses">
+                          Dashboard
+                        </ActiveLink>
+                      </li>
+                    )}
+                    {isStudent && (
+                      <li>
+                        <ActiveLink to="/dashboard/myselectedclass">
+                          Dashboard
+                        </ActiveLink>
+                      </li>
+                    )}
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
+        {/* Mobile Navlinks end */}
+
+        <div className="flex items-center">
+          <div className="hidden md:block mr-4">
+            {/* <ThemeToggle></ThemeToggle> */}
+            <DarkMoodToggle></DarkMoodToggle>
+          </div>
+          <div>
+            {user ? (
+              <>
+                <div className="flex">
+                  <img
+                    className="w-12 rounded-full mr-4 hidden md:block"
+                    src={user.photoURL || NonUserPhoto}
+                    alt=""
+                  />
+                  <button
+                    onClick={handleLogout}
+                    className="text-[17px] px-4 py-2 rounded-sm bg-[#EE9322] hover:bg-[#0E0C1A] text-white border-none"
+                  >
+                    logOut
+                  </button>
+                </div>
+              </>
+            ) : (
+              <Link to="/login">
+                <button className="text-[17px] px-4 py-2 rounded-sm bg-[#EE9322] hover:bg-[#0E0C1A] text-white border-none">
+                  Login
+                </button>
+              </Link>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
